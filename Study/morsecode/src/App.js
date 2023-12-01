e3c62d6a051b91e88752a2a39b86cbb6b38b08d1
Sergey Karpov <cliovox0409@gmail.com>
2023-12-01 11:44:39 -0500

    add converter, implement timing hold logic

--- Study/morsecode/src/App.js
+++ Study/morsecode/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from 'react';
+import {Text} from 'react-native';
+import { convert } from './code2text.js';
 import './App.css';
 
 function App() {
@@ -10,56 +12,91 @@ function App() {
 
   const [downTime, setDownTime] = useState(0);
   const [upTime, setUpTime] = useState(0);
+  const [holdTime, setHoldTime] = useState(0);
 
-  function setTime() {
-    return new Date().getTime();}
+
+  function setTime(what) {
+    let time = new Date().getTime() - 1701380000000;
+    console.log("   -> "+what+ " " + time);
+    return time;
+  }
 
   const playAudio = () => {
-    let path = require("./beep.mp3").default;
+    let path = require("./dash.mp3").default;
     const audio = new Audio(path);
-   const audioPromise = audio.play();
-    if (audioPromise !== undefined) {
-        audioPromise
-            .then(() => {
-                console.log("works");
-            })
-            .catch((err) => {
-                console.info(err);
-            });
-    }
+    
+    console.log("playing beep");
+
+    audio.play();
+
+//    const audioPromise = audio.play();
+//    if (audioPromise !== undefined) {
+//        audioPromise
+//            .then(() => {
+//                console.log("works");
+//            })
+//            .catch((err) => {
+//                console.info(err);
+//            });
+//    }
   }
 
   function handleDown() {
+    console.info("------------------"+convert("...")+convert("---")+convert("...")+"---------------------------");
+  
     setDownMessage(downAction + " down");
-    setDownTime(setTime());
 
-    baseTime = downTime;
-    console.info("Mouse down:" + downTime + "   " + baseTime);
+    let dir = "Down";
+    setDownTime(setTime(dir));
+
+//  console.info("Mouse down:" + downTime);
   }  
   function handleUp() {
     setUpMessage(upAction + " up");
     
-    console.log("playing beep");
+//   console.log("Mouse up: "+ upTime);
+  
     playAudio();
 
-    setUpTime(setTime());
-    let delay = upTime - baseTime;
+    let up = setTime("Up");
     
-    console.log("Mouse down: " + upTime + " - " + baseTime + " = " + delay);
-  }  
+    let holdTime = up - downTime;
+    let char = convert(morseChar(holdTime));
+
+    Message(morseMessage + char);
+
+    console.log("Mouse up: "+ char + "  " + morseMessage) ;
+  } 
+  
+  function morseChar(hold)
+  {
+     if(hold < 120){
+        return '.';
+     }   
+     if(hold > 200){
+        return '-';
+     }   
+     return '';
+  }
    
   return (
     <div className="App">
       <label>
         Write your post:
         <br/><br/><br/>
-        <textarea name="postContent" rows={4} cols={40}>{morseMessage}</textarea>
+        <textarea name="postContent" rows={20} cols={60}>{morseMessage}</textarea>
       </label> 
       <br /><br /><br /><br />
+        <Text numberOfLines={5}  >{morseMessage}</Text>
+      <br/><br/><br/>
       <button onMouseDown={() => handleDown()} onMouseUp={() => handleUp()}>Morse Key</button>
       <br />
     </div>
   );
+
+
 }
+
+
 
 export default App;
