3d9b66a324f5ed466df56aee0b3239e13e8004f6
Sergey Karpov <cliovox0409@gmail.com>
2023-12-28 08:10:26 -0500

    add working web view blazor wpf

---
+++ AspDotNetDemo/BlazorWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Clio.Demo.Abstraction.Interface;
+using Clio.Demo.Core.Component.Gateway;
+using Clio.Demo.DataManagement.Processor.EqD.DataModel;
+using Clio.Demo.DataManagement.Processor.EqD;
+using Clio.Demo.DataPresentation.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using Radzen;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using System.IO;
+
+namespace BlazorWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        IConfiguration _configuration;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ServiceCollection serviceCollection = new ServiceCollection();
+
+            _configuration = createConfiguration();
+
+            serviceCollection.AddSingleton<IConfiguration> (_configuration);
+
+            serviceCollection.AddWpfBlazorWebView();
+
+            serviceCollection.AddHttpClient();
+            serviceCollection.AddTransient<ISqlGateway, SqlDapperGateway>();
+
+            serviceCollection.AddScoped<IAccountData, AccountData>();
+            serviceCollection.AddScoped<IOpenPositionData, OpenPositionData>();
+            serviceCollection.AddScoped<ITradeBlotterData, TradeBlotterData>();
+
+            serviceCollection.AddScoped<EqDerivProcessor>();
+            serviceCollection.AddScoped<EqDerivViewModel>(); // ViewModel is to be available for injection into razor components
+
+            serviceCollection.AddRadzenComponents();
+
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+
+        private IConfiguration createConfiguration()
+        {
+            return new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                                             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                                             .Build();
+        }
+    }
+}
